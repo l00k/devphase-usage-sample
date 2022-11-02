7c33a1a3dade3367d2ccdc616713394120e419d2
l00k <l00k+github@protonmail.com>
2022-11-02 17:46:54 +0100

    update

--- devphase.config.ts
+++ devphase.config.ts
@@ -1,5 +1,15 @@
 import { ConfigOption } from 'devphase';
 
-const config : ConfigOption = {};
+const config : ConfigOption = {
+    stack: {
+        node: {
+            binary: 'node_modules/devphase/phala-dev-stack/bin/node',
+            port: 9955,
+        },
+        pruntime: {
+            port: 8010
+        }
+    }
+};
 
 export default config;
