cf862b5e29bf3525f7f10aa614f0ae7878c039ad
l00k <l00k+github@protonmail.com>
2022-11-06 22:06:39 +0100

    update to new release

--- typings/Flipper.ts
+++ typings/Flipper.ts
@@ -1,19 +1,50 @@
-import * as DevPhase from "devphase";
+import type * as PhalaSdk from "@phala/sdk";
+import type * as DevPhase from "devphase";
+import type * as DPT from "devphase/etc/typings";
+import type { ContractCallResult, ContractQuery } from "@polkadot/api-contract/base/types";
+import type { ContractCallOutcome, ContractOptions } from "@polkadot/api-contract/types";
+import type { Codec } from "@polkadot/types/types";
 
 export namespace Flipper {
-    export interface MapMessageQuery extends DevPhase.MapMessageQuery {
-        get: DevPhase.ContractQuery;
+    type InkEnv_Types_AccountId = any;
+
+    /** */
+    /** Queries */
+    /** */
+    namespace ContractQuery {
+        export interface Get extends DPT.ContractQuery {
+            (certificateData: PhalaSdk.CertificateData, options: ContractOptions): DPT.CallResult<DPT.CallOutcome<DPT.IBool>>;
+        }
     }
 
-    export interface MapMessageTx extends DevPhase.MapMessageTx {
-        flip: DevPhase.ContractTx;
+    export interface MapMessageQuery extends DPT.MapMessageQuery {
+        get: ContractQuery.Get;
     }
 
-    export declare class Contract extends DevPhase.Contract {
+    /** */
+    /** Transactions */
+    /** */
+    namespace ContractTx {
+        export interface Flip extends DPT.ContractTx {
+            (options: ContractOptions): DPT.SubmittableExtrinsic;
+        }
+    }
+
+    export interface MapMessageTx extends DPT.MapMessageTx {
+        flip: ContractTx.Flip;
+    }
+
+    /** */
+    /** Contract */
+    /** */
+    export declare class Contract extends DPT.Contract {
         get query(): MapMessageQuery;
         get tx(): MapMessageTx;
     }
 
+    /** */
+    /** Contract factory */
+    /** */
     export declare class Factory extends DevPhase.ContractFactory {
         instantiate<T = Contract>(constructor: "new", params: [boolean], options?: DevPhase.InstantiateOptions): Promise<T>;
         instantiate<T = Contract>(constructor: "default", params: never[], options?: DevPhase.InstantiateOptions): Promise<T>;
